2e7a5bb59895ab8c12377b733dfffbca36d81661
melinachenani <melina.ch98@gmail.com>
2023-09-20 14:02:45 +0330

    test ten

--- index.js
+++ index.js
@@ -142,13 +142,27 @@ btns.forEach((btn) => {
 });
 
 
-const jsonServer = require("json-server"); // importing json-server library
+// JSON Server module
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router("db.json");
+const router = jsonServer.router("db/db.json");
+
+// Make sure to use the default middleware
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 8080; //  chose port from here like 8080, 3001
 
 server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
 server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
 
-server.listen(port);
+// Export the Server API
+module.exports = server;
